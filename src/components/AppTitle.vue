e5d0f2fc2ad9a280ffaddfe9fb806f326e10ee04
lollomrt <lorenzomartini.mrt@outlook.it>
2023-01-10 17:36:27 +0100

    Creazione componente AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: AppTitle,
+    data() {
+        return {
+            message: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
+
+<style>
+
+</style>
